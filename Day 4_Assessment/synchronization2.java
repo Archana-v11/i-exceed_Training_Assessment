0457c92302c9a3fff8981a5eb674b5ba2b3d649f
Archana-v11 <110531490+Archana-v11@users.noreply.github.com>
2025-05-13 17:56:37 +0530

    Add files via upload

---
+++ Day 4_Assessment/synchronization2.java
@@ -0,0 +1,44 @@
+class sample
+{
+	public void display(String msg)
+	{
+		System.out.print("[");
+		System.out.print(msg);
+		System.out.print("]");		
+		this.show();
+	}
+	public void show()
+	{
+		System.out.println("HIII");
+	}
+}
+class demo extends Thread
+{
+	Thread t=new Thread(this);
+	sample s;
+	String msg;
+	demo(sample s,String msg)
+	{
+		this.s=s;
+		this.msg=msg;
+		t.start();
+	}
+	public void run()
+	{
+		synchronized(s)
+		{
+			s.display(msg);
+		}
+	}
+}
+
+class synchronization2
+{
+	public static void main(String args[])
+	{
+		sample s=new sample();
+		demo d=new demo(s,"Welcome");
+		demo d1=new demo(s,"to");
+		demo d2=new demo(s,"i-exceed");
+	}
+}
