36b3afcb5dc66eae56c36558e07078162ea03c68
Archana-v11 <110531490+Archana-v11@users.noreply.github.com>
2025-05-29 10:32:19 +0530

    Add files via upload

---
+++ Day 15_Assessment/list_view.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main()
+{
+  runApp(MyListViewApp());
+}
+class MyListViewApp extends StatelessWidget {
+  const MyListViewApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("List View Demo"),
+          centerTitle: true,
+          backgroundColor: Colors.cyan,
+        ),
+        body: ListView(
+          scrollDirection: Axis.vertical,
+          physics: BouncingScrollPhysics(),
+          //reverse: true,
+          //padding: EdgeInsets.all(50.0), -> this is for padding the entire container
+          //itemExtent: 200.0, // changes the size of all the container
+          //prototypeItem:const SizedBox(height:300.0,width:200.0), -> this is also for padding the entire container
+
+          children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              height: 150,
+              width: 250,
+              child: Center(child: Text("Container 1"),),
+              color: Colors.lime.shade300,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              height: 150,
+              width: 2500,
+              alignment: Alignment.center,
+              child: Text("Container 2",
+              style: TextStyle(
+                color: Colors.white
+              ),),
+              color: Colors.purpleAccent.shade200,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              height: 150,
+              width: 100,
+              alignment: Alignment.center,
+              child: Text("Container 3"),
+              color: Colors.deepOrangeAccent.shade100,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              height: 150,
+              width: 100,
+              alignment: Alignment.center,
+              child: Text("Container 4"),
+              color: Colors.amberAccent,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              height: 150,
+              width: 100,
+              alignment: Alignment.center,
+              child: Text("Container 5"),
+              color: Colors.greenAccent,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              height: 150,
+              width: 100,
+              alignment: Alignment.center,
+              child: Text("Container 6"),
+              color: Colors.blueGrey,
+            ),
+          ),
+        ],
+
+
+        )
+      ),
+    );
+  }
+}
