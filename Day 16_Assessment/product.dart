f3f2778336e3001dccf77236ee881947a127fb58
Archana-v11 <110531490+Archana-v11@users.noreply.github.com>
2025-05-29 17:07:45 +0530

    Add files via upload

---
+++ Day 16_Assessment/product.dart
@@ -0,0 +1,46 @@
+class Product {
+  String? name;
+  double? price;
+  String? image;
+
+  Product({this.name, this.price, this.image});
+}
+  List pro = [
+    Product(
+      name: "Shoes",
+      price: 499.99,
+      image: "assets/images/shoes.png",
+    ),
+    Product(
+      name: "Smart Watch",
+      price: 2999.00,
+      image: "assets/images/smart_watch.png",
+    ),
+    Product(
+      name: "Wireless Earbuds",
+      price: 1299.00,
+      image: "assets/images/wireless_earbuds.png",
+    ),
+    Product(
+      name: "Hand Bags",
+      price: 899.50,
+      image: "assets/images/hand_bags.png",
+    ),
+    Product(
+      name: "Sunglasses",
+      price: 799.00,
+      image: "assets/images/sunglass.png",
+    ),
+    Product(
+      name: "T-Shirt",
+      price: 399.99,
+      image: "assets/images/tshirts.png",
+    ),
+    Product(
+      name: "Laptops",
+      price: 59999.99,
+      image: "assets/images/laptop.png",
+    ),
+  ];
+
+
