de7a37706bb811c16a32630c4d73d74a83264d3b
Archana-v11 <110531490+Archana-v11@users.noreply.github.com>
2025-06-04 11:21:42 +0530

    Update sizedbox.dart

--- Day 20_Assessment/sizedbox.dart
+++ Day 20_Assessment/sizedbox.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MySizedBoxDemo());
+  runApp(MySizedBoxDemo());
 }
 
 class MySizedBoxDemo extends StatelessWidget {
@@ -13,66 +13,96 @@ class MySizedBoxDemo extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-            title: Center(child: Text("SizedBox Variations Demo")),
-            backgroundColor: Colors.lightBlueAccent[100],
+          title: Center(child: Text("SizedBox Variations Demo")),
+          backgroundColor: Colors.lightBlueAccent[100],
         ),
         body: Padding(
-          padding:  EdgeInsets.all(16.0),
-          child: ListView(
+          padding: EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
 
               // Standard SizedBox (spacing)
-              Text("1. SizedBox (Vertical Space)", style: TextStyle(fontWeight: FontWeight.bold)),
-              SizedBox(height: 20,),
+              Text(
+                "1. SizedBox (Vertical Space)",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 20),
               Container(height: 100, color: Colors.yellowAccent),
               SizedBox(height: 10),
               Container(height: 100, color: Colors.indigoAccent),
+
+              SizedBox(height: 15),
               Divider(),
+              SizedBox(height: 15),
 
               // SizedBox.fromSize
-              Text("2. SizedBox.fromSize", style: TextStyle(fontWeight: FontWeight.bold)),
-              SizedBox(height: 10),
-              SizedBox.fromSize(
-                size:  Size(120, 60),
-                child: ElevatedButton(
-                  onPressed: () {},
-                  child:  Text("fromSize button",),
-                ),
+              Text(
+                "2. SizedBox.fromSize",
+                style: TextStyle(fontWeight: FontWeight.bold),
               ),
-              Divider(),
-
-              // SizedBox.shrink
-              Text("3. SizedBox.shrink (with ConstrainedBox)", style: TextStyle(fontWeight: FontWeight.bold)),
               SizedBox(height: 10),
-              ConstrainedBox(
-                constraints:  BoxConstraints(
-                  minWidth: 100,
-                  minHeight: 50,
-                ),
-                child: SizedBox.shrink(
+              Center(
+                child: SizedBox.fromSize(
+                  size: Size(170, 60),
                   child: ElevatedButton(
                     onPressed: () {},
-                    child:  Text("shrink button"),
+                    child: Text("fromSize button"),
                   ),
                 ),
               ),
-              Divider(),
 
-              // SizedBox.expand
-              Text("4. SizedBox.expand (with ConstrainedBox)", style: TextStyle(fontWeight: FontWeight.bold)),
+              SizedBox(height: 15),
+              Divider(),
+              SizedBox(height: 15),
+
+              // SizedBox.shrink with ConstrainedBox
+              Text(
+                "3. SizedBox.shrink (with ConstrainedBox)",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
               SizedBox(height: 10),
-              ConstrainedBox(
-                constraints: BoxConstraints(
-                  maxHeight: 150,
-                  maxWidth: 150,
-                ),
-                child: SizedBox.expand(
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    child: Text("expand button"),
+              Center(
+                child: ConstrainedBox(
+                  constraints: BoxConstraints(
+                    minWidth: 170,
+                    minHeight: 50,
+                  ),
+                  child: SizedBox.shrink(
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child:Text("shrink button"),
+                    ),
                   ),
                 ),
               ),
+
+              SizedBox(height: 15),
+              Divider(),
+              SizedBox(height: 15),
+
+              // SizedBox.expand with ConstrainedBox
+              Text(
+                "4. SizedBox.expand (with ConstrainedBox)",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 10),
+              Center(
+                child: ConstrainedBox(
+                  constraints: BoxConstraints(
+                    maxWidth: 100,
+                    maxHeight: 100,
+                  ),
+                  child: SizedBox.expand(
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: Text("expand button"),
+                    ),
+                  ),
+                ),
+              ),
+
+              SizedBox(height: 30),
             ],
           ),
         ),
